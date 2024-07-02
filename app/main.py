4fc766aa0174f53ffe222cb8f263f1bc77ebfe6e
Joseph Bouqdib <joseph@bouqdib.com>
2024-07-02 15:35:58 +0100

    respond with 200

--- app/main.py
+++ app/main.py
@@ -11,10 +11,7 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.accept() # wait for client
 
-    # respond to get request from client
-    client_socket = server_socket.accept()[0]
-    client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-    client_socket.close()
+    server_socket.send(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
