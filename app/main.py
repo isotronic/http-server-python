c4cf6a1fc137a407203a3aeec437cc850d5e8a12
Joseph Bouqdib <joseph@bouqdib.com>
2024-07-02 16:24:51 +0100

    extract url path

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
-# Uncomment this to pass the first stage
 import socket
+
+def handle_request(request):
+    path = request.split(" ")[1]
+    if path == "/":
+        return "HTTP/1.1 200 OK\r\n\r\n"
+    else:
+        return "HTTP/1.1 404 Not Found\r\n\r\n"
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    
+  
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     
     while True:
@@ -17,7 +21,9 @@ def main():
         request = client_socket.recv(1024).decode("utf-8")
         print(f"Received request: {request}")
 
-        client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        response = handle_request(request)
+
+        client_socket.sendall(response.encode("utf-8"))
         client_socket.close()
 
 
