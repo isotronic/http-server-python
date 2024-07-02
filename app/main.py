f44b6ba11b7d215735c743b33dd6bbe5b07729e3
Joseph Bouqdib <joseph@bouqdib.com>
2024-07-02 19:31:39 +0100

    respond with body

--- app/main.py
+++ app/main.py
@@ -1,18 +1,28 @@
 import socket
 
 def handle_request(request):
+    """
+    A function that handles an HTTP request and generates an appropriate HTTP response based on the request path.
+    This function takes the request as input and returns the corresponding HTTP response.
+    """
     path = request.split(" ")[1]
     if path == "/":
         return "HTTP/1.1 200 OK\r\n\r\n"
+    elif path.startswith("/echo"):
+        (echo, ) = path.split("/echo/")[1:2] or ""
+        status = "HTTP/1.1 200 OK\r\n"
+        content_type = "Content-Type: text/plain\r\n"
+        content_length = f"Content-Length: {len(path.split('/echo/')[1])}\r\n"
+
+        return status + content_type + content_length + "\r\n" + echo
     else:
         return "HTTP/1.1 404 Not Found\r\n\r\n"
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    print("Logs from the program will appear here!")
   
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=False)
     
     while True:
         client_socket, client_address = server_socket.accept()
